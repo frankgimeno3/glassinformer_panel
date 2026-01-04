88f8b6ce53196db500774336641d2be59f21d141
frank gimeno <frankgimeno3@gmail.com>
2026-01-04 10:56:06 +0100

    correccion de route.js

--- app/api/validate-token/route.js
+++ app/api/validate-token/route.js
@@ -1,32 +1,30 @@
-import {COGNITO} from "../../../env.js";
-import {verifyAccessToken, verifyIdToken} from "../../../server/features/authentication/AuthenticationService.js";
-
-// Validar que las variables de entorno están definidas
-if (!COGNITO?.USER_POOL_ID || !COGNITO?.CLIENT_ID) {
-    throw new Error("Cognito USER_POOL_ID or CLIENT_ID is missing or undefined.");
-}
-
+import { COGNITO } from "../../../env.js";
+import { verifyAccessToken, verifyIdToken } from "../../../server/features/authentication/AuthenticationService.js";
 
 export async function POST(request) {
-    try {
-        const baseKey = `CognitoIdentityServiceProvider.${COGNITO.CLIENT_ID}`;
-        const username = request.cookies.get(`${baseKey}.LastAuthUser`)?.value;
-
-        if (!username) {
-            return new Response("Missing username cookie", {status: 400});
-        }
-
-        const idToken = request.cookies.get(`${baseKey}.${username}.idToken`)?.value;
-        const accessToken = request.cookies.get(`${baseKey}.${username}.accessToken`)?.value;
-
-        if (!idToken || !accessToken) {
-            return new Response("Missing token(s)", {status: 400});
-        }
-
-        await Promise.all([verifyIdToken(idToken), verifyAccessToken(accessToken)]);
-
-        return new Response("Ok", {status: 200});
-    } catch (error) {
-        return new Response("Invalid Token", {status: 401});
+  try {
+    if (!COGNITO || !COGNITO.USER_POOL_ID || !COGNITO.CLIENT_ID) {
+      return new Response("Cognito USER_POOL_ID or CLIENT_ID is missing or undefined.", { status: 500 });
     }
+
+    const baseKey = `CognitoIdentityServiceProvider.${COGNITO.CLIENT_ID}`;
+    const username = request.cookies.get(`${baseKey}.LastAuthUser`)?.value;
+
+    if (!username) {
+      return new Response("Missing username cookie", { status: 400 });
+    }
+
+    const idToken = request.cookies.get(`${baseKey}.${username}.idToken`)?.value;
+    const accessToken = request.cookies.get(`${baseKey}.${username}.accessToken`)?.value;
+
+    if (!idToken || !accessToken) {
+      return new Response("Missing token(s)", { status: 400 });
+    }
+
+    await Promise.all([verifyIdToken(idToken), verifyAccessToken(accessToken)]);
+
+    return new Response("Ok", { status: 200 });
+  } catch (error) {
+    return new Response("Invalid Token", { status: 401 });
+  }
 }
